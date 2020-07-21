53ed8455a3361d4c3534a0f7a9555634a061ced1
sachix1001 <sachix1001@gmail.com>
2020-07-21 10:07:52 +1000

    add img

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { API } from "aws-amplify";
+import { API, Storage } from "aws-amplify";
 import "./App.css";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import { listTodos } from "./graphql/queries";
@@ -20,6 +20,16 @@ function App() {
 
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listTodos });
+    const notesFromAPI = apiData.data.listTodos.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const image = await Storage.get(note.image);
+          note.image = image;
+        }
+        return note;
+      })
+    );
     setNotes(apiData.data.listTodos.items);
   }
 
@@ -29,6 +39,10 @@ function App() {
       query: createNoteMutation,
       variables: { input: formData },
     });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
     setNotes([...notes, formData]);
     setFormData(initialFormState);
   }
@@ -40,6 +54,14 @@ function App() {
       query: deleteNoteMutation,
       variables: { input: { id } },
     });
+  }
+
+  async function onChange(e) {
+    if (!e.target.files[0]) return;
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
   }
 
   return (
@@ -57,6 +79,7 @@ function App() {
         placeholder="NoteDescription"
         value={formData.description}
       />
+      <input type="file" onChange={onChange} />
       <button onClick={createNote}>Create Note</button>
       <div style={{ marginBottom: 30 }}>
         {notes.map((note) => (
@@ -64,6 +87,9 @@ function App() {
             <h2>{note.name}</h2>
             <p>{note.description}</p>
             <button onClick={() => deleteNote(note)}>Delete note</button>
+            {
+            note.image && <img src={note.image} style={{ width: 400 }} />
+            }
           </div>
         ))}
       </div>
